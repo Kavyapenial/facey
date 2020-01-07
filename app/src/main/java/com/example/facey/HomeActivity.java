46a6ae756611ef40d82fe788b939de3819cd5b25
Raison <raison@entri.me>
2020-01-07 14:30:46 +0530

    Add firebase

---
+++ app/src/main/java/com/example/facey/HomeActivity.java
@@ -0,0 +1,35 @@
+package com.example.facey;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class HomeActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+
+            Log.d("Firebase", currentUser.getDisplayName());
+            startActivity(new Intent(this, AttendenceSelect.class));
+        }else{
+            startActivity(new Intent(this, MainActivity.class));
+        }
+    }
+}
